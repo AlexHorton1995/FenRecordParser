cef91f619043c9083a42f8897d0d31eccf4ac9f6
Thwacksmith <116522524+Thwacksmith@users.noreply.github.com>
2023-06-18 17:52:58 -0500

    Add files via upload

---
+++ Blitboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FenRecordParser;
+
+[StructLayout(LayoutKind.Sequential)]
+struct Blitboard
+{
+    public ulong pawns;
+    public ulong knights;
+    public ulong bishops;
+    public ulong rooks;
+    public ulong queens;
+    public ulong kings;
+    public ulong white;
+    public ulong black;
+    public uint sideToMove;
+    public uint castlingRights;
+    public uint epTargetSquare;
+    public uint halfmoveClock;
+    public uint fullmoveNumber;
+}
